c0a6c112c0e13c4cd6d4083ad0806a0d4a453adf
Raff <raffine710@gmail.com>
2022-09-23 00:22:20 -0300

    se modificaron archivos html, css y js

--- assets/js/javascrip.js
+++ assets/js/javascrip.js
@@ -21,26 +21,22 @@ $(document).ready(function(){
         });
       } // End if
     });
+   
 
- 
 
     window.addEventListener("scroll", function(){
-
-        var header = document.getElementById("header")
-    
+        var header = document.querySelector("header")
         if (window.scrollY>0) {
-    
-            header.style.backgroundColor="blue";
-    
+            header.style.backgroundColor="#198754";
         }else{
-    
             header.style.backgroundColor="transparent";
     
         }
     
     }) 
-
+  }); 
   
-
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
     
-});  
+    
