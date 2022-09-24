b2f4ab120a14d69e2ac7301be601bdb97467569a
Raff <raffine710@gmail.com>
2022-09-23 22:48:44 -0300

    Modificaciones a archivos html, css, js e img

--- assets/js/javascrip.js
+++ assets/js/javascrip.js
@@ -1,30 +1,4 @@
-$(document).ready(function(){
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
-  
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-  
-        // Store hash
-        var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 500, function(){
-  
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      } // End if
-    });
-   
-
-
-    window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function(){
         var header = document.querySelector("header")
         if (window.scrollY>0) {
             header.style.backgroundColor="#198754";
@@ -34,7 +8,7 @@ $(document).ready(function(){
         }
     
     }) 
-  }); 
+
   
     const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
     const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
