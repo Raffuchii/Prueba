3a70aa147596dd69823a4953063767169807156d
Raff <raffine710@gmail.com>
2022-09-23 23:09:03 -0300

    formateo del archivo html y pequeña nota en el archivo js

--- assets/js/javascrip.js
+++ assets/js/javascrip.js
@@ -1,3 +1,5 @@
+
+// Creo que esta parte el visor de GitHud no logra visualizarlo pero el de VSC si (???)
 window.addEventListener("scroll", function(){
         var header = document.querySelector("header")
         if (window.scrollY>0) {
@@ -8,6 +10,7 @@ window.addEventListener("scroll", function(){
         }
     
     }) 
+
 
   
     const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
